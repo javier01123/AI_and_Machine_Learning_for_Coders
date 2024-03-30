d062764ccb31f231c22fc8c689acc005a895ab6d
Javier Andrade <javier01123@gmail.com>
2024-03-30 16:53:38 -0600

    comments explaining chapter 2 exercise

--- src/chapter_2/fashion.py
+++ src/chapter_2/fashion.py
@@ -1,7 +1,10 @@
+import sys
 import tensorflow as tf
 data = tf.keras.datasets.fashion_mnist
 
+#images as 2d arrays
 (training_images, training_labels), (test_images, test_labels) = data.load_data()
+# print('first image as array:',training_images[0])
 
 training_images = training_images/255.0
 test_images = test_images/255.0
@@ -25,15 +28,21 @@ model.compile(optimizer = 'adam',
               loss='sparse_categorical_crossentropy',
               metrics=['accuracy'])
 
-model.fit(training_images, training_labels, epochs=4)
+model.fit(training_images, training_labels, epochs=5)
+print('###############################################')
 
 print('EVALUATION:')
+#test model vs the testing data
 model.evaluate(test_images, test_labels)
+print('###############################################')
 
-print('CLASSIFICATIONS:')
+print('PREDICT:')
+
+#classify 10k test images(byte arrays)
+classifications = model.predict(test_images)
+print('images classified count:', len(classifications))
+print('neurons percentages for first image:',classifications[0])
+print('label for first image:',test_labels[0])
 #These are the probabilities
 #that the image matches the label at that particular index. So, what the neural network
 #is reporting is that there’s a 91.4% chance that the item of clothing at index 0 is label 9.
-classifications = model.predict(test_images)
-print(classifications[0])
-print(test_labels[0])
